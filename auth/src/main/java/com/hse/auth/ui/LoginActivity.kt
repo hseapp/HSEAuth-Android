7f083528a0e291ce310937d8455e9362961b8f25
g.shchipin <g.shchipin@corp.vk.com>
2021-02-03 20:36:29 +0300

    animation fix

--- auth/src/main/java/com/hse/auth/ui/LoginActivity.kt
+++ auth/src/main/java/com/hse/auth/ui/LoginActivity.kt
@@ -1,9 +1,11 @@
 package com.hse.auth.ui
 
 import android.accounts.AccountManager
+import android.animation.ValueAnimator
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import android.widget.FrameLayout
 import com.google.android.material.bottomsheet.BottomSheetBehavior
@@ -11,6 +13,7 @@ import com.hse.auth.R
 import com.hse.auth.ui.accountmanager.AccountManagerFragment
 import com.hse.auth.ui.credentials.WebViewCredentialsFragment
 import com.hse.auth.utils.AuthConstants
+import com.hse.core.common.color
 import com.hse.core.navigation.Navigation
 import com.hse.core.navigation.NavigationCallback
 import com.hse.core.ui.BaseActivity
@@ -21,6 +24,7 @@ import net.danlew.android.joda.JodaTimeAndroid
 class LoginActivity : BaseActivity(), NavigationCallback {
 
     lateinit var bottomSheetBehavior: BottomSheetBehavior<FrameLayout>
+    private var canFinish = false
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
@@ -31,11 +35,13 @@ class LoginActivity : BaseActivity(), NavigationCallback {
         super.onCreate(savedInstanceState)
         JodaTimeAndroid.init(this)
         setContentView(R.layout.activity_login)
-        initBottomSheetBehavior()
 
-        navigation =
-            Navigation(savedInstanceState, R.id.navHostFragment, supportFragmentManager, this)
+        window?.statusBarColor = color(R.color.auth_black_33pc)
+
+        navigation = Navigation(savedInstanceState, R.id.navHostFragment, supportFragmentManager, this)
         if (savedInstanceState == null) navigation?.switchStack(getRootTag())
+
+        initBottomSheetBehavior()
     }
 
     override fun getRootFragment(rootTag: String) = getRootFragment()
@@ -45,7 +51,7 @@ class LoginActivity : BaseActivity(), NavigationCallback {
     override fun onTopFragmentChanged(fragment: BaseFragment<*>?, rootTag: String) = Unit
 
     override fun onBackPressed() {
-        bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+        finish()
     }
 
     private fun getRootTag(): String = "LoginActivity"
@@ -59,29 +65,64 @@ class LoginActivity : BaseActivity(), NavigationCallback {
         else WebViewCredentialsFragment.newInstance(code)
     }
 
+    override fun onStart() {
+        super.onStart()
+        animateBackground(0f, 1f)
+
+        Handler().postDelayed({
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+        }, 100)
+    }
+
+    override fun finish() {
+        if (!canFinish) {
+            animateBackground(1f, 0f)
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+            Handler().postDelayed({
+                canFinish = true
+                finish()
+            }, 150)
+            return
+        }
+
+        super.finish()
+        overridePendingTransition(0, 0)
+    }
+
+    private fun animateBackground(from: Float = 0f, to: Float = 1f) {
+       val background = container?.background ?: return
+       ValueAnimator.ofFloat(from, to).run {
+           duration = 150
+           addUpdateListener {
+               val value = it.animatedValue as Float
+               background.alpha = (255 * value).toInt()
+           }
+           start()
+       }
+    }
+
     private fun initBottomSheetBehavior() {
         bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
         // Expanded by default
-        bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
         bottomSheetBehavior.skipCollapsed = true
         bottomSheetBehavior.addBottomSheetCallback(object :
             BottomSheetBehavior.BottomSheetCallback() {
             override fun onStateChanged(bottomSheet: View, newState: Int) {
                 if (newState == BottomSheetBehavior.STATE_HIDDEN) {
                     finish()
-                    overridePendingTransition(0, 0)
                 }
             }
 
-            override fun onSlide(bottomSheet: View, slideOffset: Float) {
-
-            }
+            override fun onSlide(bottomSheet: View, slideOffset: Float)  = Unit
         }
         )
         bottomSheet.setOnClickListener {
-            bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+            finish()
         }
     }
+
+//    private fun finish
 
     companion object {
         fun launch(
